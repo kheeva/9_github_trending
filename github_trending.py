cd44731afcf5e73c76555515557dededc982f33c
kheeva <kheeva@ya.ru>
2017-12-07 12:49:29 +0300

    new issues list

--- github_trending.py
+++ github_trending.py
@@ -10,8 +10,16 @@ def get_trending_repositories(top_size):
     return requests.get(url, params=payload).json()['items'][:top_size]
 
 
+def get_open_issues_amount(repo_owner, repo_name):
+    url = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner,
+                                                             repo_name)
+    return requests.get(url).json()
+
+
 if __name__ == '__main__':
     top_size = 20
     print('There are top {} repos in https://github.com:'.format(top_size))
     for repo in get_trending_repositories(top_size):
-        print(repo['html_url'], 'open_issues:', repo['open_issues_count'])
+        oi = get_open_issues_amount(repo['owner']['login'],
+                                              repo['name'])
+        print(repo['html_url'], 'open_issues:', oi)
